b19c6fc4114c0abd3770a21ccea7a4eb699de5c4
opilane <opilane@HP-CZC2349HT0>
2026-01-08 14:03:54 +0200

    kõik on tehtud

---
+++ Interaktiivne_Jõuluõnnitluste_Süsteem/tööfail.py
@@ -0,0 +1,34 @@
+from os import wait
+import re
+
+
+def kirjuta_failisse(failinimi:str,loend:list):
+    while True:
+        reziim = input("Sisesta faili avamise reziim(w - kirjutamine, a - lisamine): ")
+        if reziim not in ["w","a"]:
+            print("Vale reziim! Proovi uuesti.")
+        else:
+            break
+    for i in range(5):
+        element = input("Sisesta rida, mida faili lisada: ")
+        loend.append(element)
+    with open(failinimi+".txt",reziim,encoding="utf-8") as f:
+        for rida in loend:
+            f.write(rida+"\n")
+
+def loe_failist(failinimi:str)->list:
+    with open(failinimi+".txt","r",encoding="utf-8") as f:
+        for rida in f:
+            loend.append(rida.strip())
+        return loend
+
+
+loend=["Rida 1  ", "Rida 2"]
+failinimi = input("Sisesta faili nimi (ilma laiendita)")
+kirjuta_failisse(failinimi,loend)
+sisu=loe_failist(failinimi)
+print("Faili sisu: ")
+print(sisu)
+for rida in sisu:
+    print(rida)
+
